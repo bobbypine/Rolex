217bd6830afa447795f23e4d24d79bdfa661a702
Bobby Pine <rob.rudary@gmail.com>
2021-03-28 12:20:09 -0400

    Added class and def

--- WebScraping.py
+++ WebScraping.py
@@ -1,38 +1,38 @@
-import bs4
 import requests
-import webbrowser
 
-personal = 'XojKdiU2bbMb1w'
 
-secret = 'ctok-4bh8PtyT9SYpG_ILLZdiojtTQ'
+class Rolex:
+    def __init__(self):
+        self.personal = 'XojKdiU2bbMb1w'
+        self.secret = 'ctok-4bh8PtyT9SYpG_ILLZdiojtTQ'
+        # note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
+        self.auth = requests.auth.HTTPBasicAuth(self.personal, self.secret)
+        # here we pass our login method (password), username, and password
+        self.data = {'grant_type': 'password',
+            'username': 'Bobby_Pine',
+            'password': 'girr*ivY1'}
+        # setup our header info, which gives reddit a brief description of our app
+        self.headers = {'User-Agent': 'MyBot/0.0.1'}
+        # send our request for an OAuth token
+        self.res = requests.post('https://www.reddit.com/api/v1/access_token',
+                    auth=self.auth, data=self.data, headers=self.headers)
+        # convert response to JSON and pull access_token value
+        self.TOKEN = self.res.json()['access_token']
 
-# note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
-auth = requests.auth.HTTPBasicAuth(personal, secret)
+    def scrape(self):
+        # add authorization to our headers dictionary
+        authheaders = {**self.headers, **{'Authorization': f"bearer {self.TOKEN}"}}
+        # while the token is valid (~2 hours) we just add headers=headers to our requests
+        requests.get('https://oauth.reddit.com/api/v1/me', headers=authheaders)
+        res = requests.get("https://oauth.reddit.com/r/rolex/new",
+                   headers=authheaders, params={'limit':'100'})
+        for post in res.json()['data']['children']:
+            if 'call ' in post['data']['title'].lower():
+                print('Title: {} Date: {} Picture: {}'. format(post['data']['title'], post['data']['created'], post['data']['thumbnail']))
+            if 'called' in post['data']['title'].lower():
+                print('Title: {} Date: {} Picture: {}'. format(post['data']['title'], post['data']['created'], post['data']['thumbnail']))
 
-# here we pass our login method (password), username, and password
-data = {'grant_type': 'password',
-        'username': 'Bobby_Pine',
-        'password': 'rjr2990'}
 
-# setup our header info, which gives reddit a brief description of our app
-headers = {'User-Agent': 'MyBot/0.0.1'}
-
-# send our request for an OAuth token
-res = requests.post('https://www.reddit.com/api/v1/access_token',
-                    auth=auth, data=data, headers=headers)
-
-# convert response to JSON and pull access_token value
-TOKEN = res.json()['access_token']
-
-# add authorization to our headers dictionary
-headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
-
-# while the token is valid (~2 hours) we just add headers=headers to our requests
-requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
-
-res = requests.get("https://oauth.reddit.com/r/rolex/new",
-                   headers=headers, params={'limit':'1000'})
-
-for post in res.json()['data']['children']:
-    if 'call' in post['data']['title'].lower():
-        print('Title: {} Date: {}'. format(post['data']['title'], post['data']['created']))
+if __name__ == '__main__':
+    test = Rolex()
+    test.scrape()
