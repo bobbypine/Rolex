030da29e755073ab2def5d99e380726f6a0644e3
Rudary <D1RJR03@rb.win.frb.org>
2021-07-19 08:10:57 -0400

    Updates

--- Authorized_Dealers.py
+++ Authorized_Dealers.py
@@ -1,21 +1,38 @@
 import requests
-import time
 import urllib3
 from bs4 import BeautifulSoup
 import datetime
+import pandas as pd
 
 urllib3.disable_warnings()
 
 
 def ads():
-    starttime = time.time()
+    data = pd.DataFrame()
     url = 'https://www.rolex.com/rolex-dealers/unitedstates.html#mode=list&placeId=ChIJCzYy5IS16lQRQrfeQ5K5Oxw'
     response = requests.get(url=url, verify=False)
     soup = BeautifulSoup(response.text, 'html.parser')
     name_table = soup.findAll('p', {"class" : "sc-pRhbc ePDsLp"})
     address_table = soup.findAll('address', {"class" : "sc-oTzDS fotNMM"})
-    # for x in address_table:
-        # print(x.text)
-    print('{}/{}: {}'.format(datetime.date.today().month, datetime.date.today().year, len(name_table)))
+    data = data.append(name_table)
+    data['Address'] = address_table
+    data.rename(columns={0:'Name'}, inplace=True)
+    data.sort_values('Name', ascending=True, inplace=True)
+    print(data)
 
-ads()
+
+def adcount():
+    file = open('AD_Count.txt', 'a')
+    url = 'https://www.rolex.com/rolex-dealers/unitedstates.html#mode=list&placeId=ChIJCzYy5IS16lQRQrfeQ5K5Oxw'
+    response = requests.get(url=url, verify=False)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    name_table = soup.findAll('p', {"class" : "sc-pRhbc ePDsLp"})
+    address_table = soup.findAll('address', {"class" : "sc-oTzDS fotNMM"})
+    file.write('{}/{}: {} \n'.format(datetime.date.today().month, datetime.date.today().year, len(name_table)))
+    print('File Updated.')
+    print('{}/{}: {} \r\n'.format(datetime.date.today().month, datetime.date.today().year, len(name_table)))
+    file.close()
+
+
+if __name__ == "__main__":
+    adcount()
