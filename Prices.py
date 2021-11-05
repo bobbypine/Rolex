b06379e71904edf2ee5f43c00375216e9ad7eb02
Rob <robert.j.rudary@clev.frb.org>
2021-11-05 09:20:11 -0400

    Pricing updates and code change

--- Prices.py
+++ Prices.py
@@ -5,23 +5,22 @@ import datetime
 import pandas as pd
 import numpy as np
 
-
 urllib3.disable_warnings()
 
 
 def prices(ref):
     price_list = []
-    for x in range(1 ,6):
+    for x in range(1, 6):
         url = 'https://www.chrono24.com/search/index.htm?currencyId=USD&dosearch=true&facets=condition&facets=specials&facets=usedOrNew&facets=availability&maxAgeInDays=0&pageSize=120&query=Rolex+{}&redirectToSearchIndex=true&resultview=block&searchexplain=1&showpage={}&sortorder=0&specials=102&usedOrNew=new'.format \
-            (ref ,x)
+            (ref, x)
         response = requests.get(url=url, verify=False)
         soup = BeautifulSoup(response.text, 'html.parser')
         for prices in soup.findAll('strong')[5:]:
             price_list.append(prices.text)
     data = pd.DataFrame(price_list)
-    data.rename(columns={0 :ref}, inplace=True)
-    data[ref] = data[ref].str.replace('\n', '').str.replace('$', '').str.replace(',' ,'').str.strip()
-    data = data.loc[(data[ref] != 'Price on request') & (data[ref] != 'SOLD')]
+    data.rename(columns={0: ref}, inplace=True)
+    data[ref] = data[ref].str.replace('\n', '').str.replace('$', '').str.replace(',', '').str.strip()
+    data = data.loc[(data[ref].str.isnumeric())]
     data[ref] = data[ref].astype('int')
     data[f'{ref} Listings'] = len(data)
     data['Date'] = datetime.date.today().strftime('%m-%d-%Y')
@@ -37,19 +36,21 @@ def run():
     gmt = pd.DataFrame(prices('126710BLRO'))
     op = pd.DataFrame(prices('124300'))
     ex = pd.DataFrame(prices('124270'))
+    # ndsub = pd.DataFrame(prices('124060'))
     pricing = pd.concat([sub, gmt, op, ex])
     median = pd.pivot_table(pricing, index='Date', values=['126610LN', '126710BLRO', '124300', '124270'],
-                              aggfunc='median')
+                            aggfunc='median')
     listings = pd.pivot_table(pricing, index='Date', values=['126610LN Listings', '126710BLRO Listings',
                                                              '124300 Listings', '124270 Listings'], aggfunc='max')
-    combined = pd.concat([median,listings], axis=1)
+    combined = pd.concat([median, listings], axis=1)
     combined['126610LN Markup'] = (combined['126610LN'] / 9150 - 1) * 100
     combined['126710BLRO Markup'] = (combined['126710BLRO'] / 9500 - 1) * 100
     combined['124300 Markup'] = (combined['124300'] / 5900 - 1) * 100
     combined['124270 Markup'] = (combined['124270'] / 6450 - 1) * 100
-    saved_data = pd.read_csv('Prices/Weekly_Median_Prices.csv', index_col = 0)
+    # combined['124060 Markup'] = (combined['124060'] / 8100 - 1) * 100
+    saved_data = pd.read_csv('Prices/Weekly_Median_Prices.csv', index_col=0)
     saved_data = pd.concat([saved_data, combined])
-    saved_data.to_csv('Prices/Weekly_Median_Prices.csv', index = 'Date')
+    saved_data.to_csv('Prices/Weekly_Median_Prices.csv', index='Date')
 
 
 if __name__ == "__main__":
