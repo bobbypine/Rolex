9420483b6a48464fb176e2fb5c4e7b98fa299e5a
Rob <rob.rudary@gmail.com>
2023-04-03 15:30:51 -0400

    Code updates for new Rolex website

--- Code/Authorized_Dealers.py
+++ Code/Authorized_Dealers.py
@@ -27,32 +27,37 @@ def ads():
               "WI": "Wisconsin", "WY": "Wyoming"}
 
     data = pd.DataFrame()
-    url = 'https://www.rolex.com/rolex-dealers/unitedstates.html#mode=list&placeId=ChIJCzYy5IS16lQRQrfeQ5K5Oxw'
+    url = 'https://www.rolex.com/en-us/store-locator/unitedstates'
     response = requests.get(url=url, verify=True)
     soup = BeautifulSoup(response.text, 'html.parser')
     names = []
     address = []
-    for x in soup.findAll('p', {"class": "sc-oUmZK YzDqU"}):
+    country = []
+    for x in soup.findAll('h2', {"class": "css-x99bf7 e89nfm54"}):
         names.append(x.text.strip())
-    for x in soup.findAll('address', {"class": "sc-pleCs iCvOAj"}):
-        address.append(x.text.strip()[:-13])
+    for x in soup.findAll('p', {"class": "css-1l2psjl e89nfm52"}):
+        countries = x.text.strip()[-13:]
+        country.append(countries)
+        address.append(x.text.strip())
     data['Name'] = names
     data['Full Address'] = address
-    data['State'] = data['Full Address'].str[:-5].str.split(' ').str[-1]
-    data.loc[data.State == '', 'State'] = data['Full Address'].str[:-5].str.split(' ').str[-2]
-    data.loc[data['Full Address'].str.split(' ').str[-3].isin(['North', 'South', 'Rhode', 'New']), 'State']\
-        = data['Full Address'].str.split(' ').str[-3:-1]
+    data['Country'] = country
+    data['State'] = data['Full Address'].str[:-18].str.split(' ').str[-1]
+    data.loc[data.State == '', 'State'] = data['Full Address'].str[:-18].str.split(' ').str[-2]
+    data.loc[data['Full Address'].str.split(' ').str[-4].isin(['North', 'South', 'Rhode', 'New']), 'State']\
+        = data['Full Address'].str.split(' ').str[-4:-2]
     data['State'] = data['State'].apply(list_convert)
-    data['State'] = data['State'].replace(states)
-    data['Zip'] = data['Full Address'].str.split(' ').str[-1]
+    data['Zip'] = data['Full Address'].str.split(' ').str[-2].str.extract('(\d+)')
     data['Address'] = data['Full Address'].apply(lambda x: re.sub(r"(\w)([A-Z])", r"\1, \2", x))
-    data['Test'] = (data.State.str.len() + 1 + data.Zip.str.len()) * -1  # finds the character length of the zip and state and removes it from address
-    data['Address'] = data.apply(lambda row: row['Address'][:row['Test']], axis=1)
+    data['Test'] = (data.State.str.len() + 1 + data.Zip.str.len() + 1 + data.Country.str.len()) * -1  # finds the character length of the zip and state and removes it from address
+    data['Address'] = data.apply(lambda row: row['Full Address'][:row['Test']], axis=1)
     data['City'] = data.Address.str.split(',').str[-1]
-    data['Test'] = (1 + data.City.str.len()) * -1  # finds length of city and removes it
+    data['City'] = data['Address'].apply(lambda x: re.findall('[A-Z][^A-Z\s]+(?:\s+\S[^A-Z\s]*)*', x)).str[-1]
+    data['Test'] = (data.City.str.len()) * -1  # finds length of city and removes it
     data['Address'] = data.apply(lambda row: row['Address'][:row['Test']], axis=1)
     data.drop(columns=['Full Address', 'Test'], inplace=True)  # drops columns no longer needed
     data = data[['Name', 'Address', 'City', 'State', 'Zip']]
+    data['State'] = data['State'].replace(states)
     data['ID'] = data.Name + data.Address + data.City + data.State + data.Zip
     data.ID = data.ID.str.replace(',', '').str.replace(' ', '').str.upper().str.strip()
     data.to_csv(f'../AD_List/Rolex_AD_List_{datetime.date.today().month}_{datetime.date.today().year}.csv', index=False)
@@ -60,11 +65,10 @@ def ads():
 
 def adcount():
     file = open('../AD_Count/AD_Count.txt', 'a')
-    url = 'https://www.rolex.com/rolex-dealers/unitedstates.html#mode=list&placeId=ChIJCzYy5IS16lQRQrfeQ5K5Oxw'
+    url = 'https://www.rolex.com/en-us/store-locator/unitedstates'
     response = requests.get(url=url, verify=True)
     soup = BeautifulSoup(response.text, 'html.parser')
-    name_table = soup.findAll('p', {"class": "sc-oUmZK YzDqU"})
-    address_table = soup.findAll('address', {"class": "sc-pleCs iCvOAj"})
+    name_table = soup.findAll('h2', {"class": "css-x99bf7 e89nfm54"})
     print(len(name_table))
     file.write('{}/{}: {} \n'.format(datetime.date.today().month, datetime.date.today().year, len(name_table)))
     print('File Updated.')
